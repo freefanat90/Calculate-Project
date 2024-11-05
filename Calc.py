82bfe7963d69256706f5e30ecbdebf6dab0efcbe
freefanat90 <freefanat90@gmail.com>
2024-11-05 18:51:31 +0300

    Создан графический интерфейс и создан дополнительный модуль с функциями для математических операций "operatorы"

---
+++ Calc.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+import operatorы as op
+
+op.add(5,4)
+
+window = Tk()
+window.title('Калькулятор')
+window.iconbitmap(default='calc.ico')
+
+line_input = Entry(window)
+line_input.grid(row=0, column=0, columnspan=4, sticky='ew')
+
+ttk.Button(window, text='1').grid(row=3, column=0)
+ttk.Button(window, text='2').grid(row=3, column=1)
+ttk.Button(window, text='3').grid(row=3, column=2)
+ttk.Button(window, text='4').grid(row=2, column=0)
+ttk.Button(window, text='5').grid(row=2, column=1)
+ttk.Button(window, text='6').grid(row=2, column=2)
+ttk.Button(window, text='7').grid(row=1, column=0)
+ttk.Button(window, text='8').grid(row=1, column=1)
+ttk.Button(window, text='9').grid(row=1, column=2)
+ttk.Button(window, text='0').grid(row=4, column=0)
+
+ttk.Button(window, text='+').grid(row=1, column=3)
+ttk.Button(window, text='-').grid(row=2, column=3)
+ttk.Button(window, text='*').grid(row=3, column=3)
+ttk.Button(window, text='/').grid(row=4, column=3)
+ttk.Button(window, text='C').grid(row=4, column=1)
+ttk.Button(window, text='=').grid(row=4, column=2)
+
+window.mainloop()
